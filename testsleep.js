db35a425516269d15cb001901eb54ec64cb531a0
chenxyzj <chenxyzj@163.com>
2021-10-09 22:25:42 +0800

    通过await实现代码的同步执行效果。

--- testsleep.js
+++ testsleep.js
@@ -1,9 +1,15 @@
-console.log('Before');
-sleep(3000);
-console.log('After');
+doAsyncJob();
 
-async function sleep(ms) {
-    await new Promise((resolve,reject) => {
+async function doAsyncJob(){
+    console.log('Before sleep');
+    await sleep(3000);
+    console.log('After sleep');
+}
+
+
+function sleep(ms) {
+    return new Promise((resolve,reject) => {
+        console.log(`sleeping for ${ms} ms`);
         setTimeout(resolve,ms);
     });
 };
