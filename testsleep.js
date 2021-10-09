a08b18579f83e32875513689865e879210ebe91c
chenxyzj <chenxyzj@163.com>
2021-10-09 22:31:52 +0800

    通过callback实现延时执行的效果。

--- testsleep.js
+++ testsleep.js
@@ -1,11 +1,7 @@
 console.log('Before sleep');
-sleep(3000)
-  .then(() => console.log('After sleep'));
+sleep(3000, () => console.log('After sleep'))
 
-
-function sleep(ms) {
-    return new Promise((resolve,reject) => {
-        console.log(`sleeping for ${ms} ms`);
-        setTimeout(resolve,ms);
-    });
+function sleep(ms,callback) {
+    console.log(`sleeping for ${ms} ms`);
+    setTimeout(callback,ms);
 };
