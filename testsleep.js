00f3a19bd9af7ac1dc9544e6f455c288392e6b17
chenxyzj <chenxyzj@163.com>
2021-10-09 22:19:55 +0800

    命名sleep函数

--- testsleep.js
+++ testsleep.js
@@ -1,7 +1,9 @@
 console.log('Before');
-(async () =>{
-    await new Promise((resolve,reject) => {
-        setTimeout(resolve,3000);
-    });
-})();
+sleep(3000);
 console.log('After');
+
+async function sleep(ms) {
+    await new Promise((resolve,reject) => {
+        setTimeout(resolve,ms);
+    });
+};
