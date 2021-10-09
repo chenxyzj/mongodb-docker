527dff68edb710d0a07827945cdb76f092d83e7d
chenxyzj <chenxyzj@163.com>
2021-10-09 22:37:52 +0800

    外部定义命名函数，函数名作参数传入sleep实现延时。

--- testsleep.js
+++ testsleep.js
@@ -1,7 +1,9 @@
 console.log('Before sleep');
-sleep(3000, function disp() {
+sleep(3000, disp); 
+
+function disp() {
     console.log('After sleep');
-}); 
+}
 
 function sleep(ms,callback) {
     console.log(`sleeping for ${ms} ms`);
