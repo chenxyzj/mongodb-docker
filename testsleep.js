9591d7f57f9dd5716c873a15d0ab946a8f39dfc2
chenxyzj <chenxyzj@163.com>
2021-10-09 22:46:43 +0800

    直接使用setTimeout和箭头函数作为参数，不自定义sleep函数。

--- testsleep.js
+++ testsleep.js
@@ -1,8 +1,4 @@
 const ms = 3000;
 console.log('Before sleep');
 console.log(`sleeping for ${ms} ms`);
-setTimeout(disp, ms);
-
-function disp() {
-    console.log('After sleep');
-}
+setTimeout(() => console.log('After sleep'), ms);
